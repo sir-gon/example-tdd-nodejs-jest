61c219df1e58b433a45f614824ecb56f0d7f0c17
Gonzalo Diaz <gonzalo@gon.cl>
2020-07-08 22:26:27 -0400

    BEHAVIOR CHANGE: new tests added.

--- src/__tests__/iamsick.test.js
+++ src/__tests__/iamsick.test.js
@@ -2,6 +2,9 @@
 import { SickException } from '../iamsick.error';
 import * as sick from '../iamsick';
 
+// Test Case data
+import * as covid19TestCase from './testCases/covid19.json';
+
 describe('I am sick', () => {
     it('Unexpected input', () => {
         expect.assertions(1)
@@ -15,7 +18,38 @@ describe('I am sick', () => {
         };
 
         expect(t).toThrow( SickException );
-    });    
+    });
+
+    it('I have some symptoms', () => {
+        expect.assertions(2);
+
+        const mySymtoms = ['fever'];
+
+        const response = sick.iAmSick(
+            covid19TestCase.mandatory,
+            covid19TestCase.optionals,
+            mySymtoms
+        );
+
+        expect((typeof response).toLowerCase()).toBe('boolean');
+        expect(response).toBe(true);
+    });
+
+    it('I don\'t have a symptom', () => {
+        expect.assertions(2);
+
+        const mySymtoms = [];
+
+        const response = sick.iAmSick(
+            covid19TestCase.mandatory,
+            covid19TestCase.optionals,
+            mySymtoms
+        );
+
+        expect((typeof response).toLowerCase()).toBe('boolean');
+        expect(response).toBe(false);
+
+    });
 
 });
   
