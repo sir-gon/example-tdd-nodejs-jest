82a52ceccb30915d4208b0fd4021322173003871
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-22 00:36:06 -0400

    [CONFIG] Jest config fixes

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
-module.exports = {
+export default {
   'verbose': true,
   'testEnvironment': 'node',
-  transform: {
-    '\\.m?jsx?$': 'jest-esm-transformer'
-  },
   'globals': {
     'NODE_ENV': 'test'
   },
   'moduleFileExtensions': ['js', 'jsx'],
   'moduleDirectories': ['node_modules', 'lambda/'],
   'collectCoverage': true,
-  'coverageReporters': ['text'],
+  'coverageReporters': [
+    'clover',
+    'json',
+    'lcov',
+    ['text', { 'skipFull': true }]
+  ],
   'coverageDirectory': 'coverage/',
-  'transformIgnorePatterns': ['/app/.cache/', '/app/node_modules'],
-  'testMatch': ['**/?(*.)+(spec|test).js?(x)'],
-  'testPathIgnorePatterns': ['<rootDir>/.cache/', '/node_modules/']
+  'testMatch': ['**/?(*.)+(spec|test).js?(x)']
 };
